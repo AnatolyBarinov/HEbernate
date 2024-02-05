266c5e22dce1068b95f901d530a2e702a70f4bb4
AnatolyBarinov <62007944+AnatolyBarinov@users.noreply.github.com>
2024-02-05 21:49:16 +0300

    first commit Auth2

---
+++ src/main/java/org/example/conroller/SecurityPersonController.java
@@ -0,0 +1,44 @@
+package org.example.conroller;
+
+import org.example.model.Person;
+import org.example.repository.PersonRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/secure")
+public class SecurityPersonController {
+
+    private PersonRepository personRepository;
+
+    public SecurityPersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @PreAuthorize("hasAuthority('READ')")
+    @GetMapping("/readValues")
+    public List<Person> readValues() {
+        return personRepository.findAll(); // Здесь мы возвращаем все значения из репозитория
+    }
+
+    @PreAuthorize("hasAuthority('WRITE')")
+    @PostMapping("/writeValue")
+    public Person writeValue(@RequestBody Person person) {
+        return personRepository.save(person); // Здесь мы сохраняем новое значение в репозиторий
+    }
+
+    @PreAuthorize("hasAnyAuthority('WRITE', 'DELETE')")
+    @DeleteMapping("/deleteValue/{id}")
+    public void deleteValue(@PathVariable Long id) {
+        personRepository.deleteById(id); // Здесь мы удаляем значение по указанному id из репозитория
+    }
+
+    @PreAuthorize("#username == authentication.name")
+    @GetMapping("/checkUsername")
+    public Person checkUsername(@RequestParam String username) {
+        // Здесь мы будем возвращать информацию о пользователе, если имя пользователя совпадает с именем аутентификации
+        return personRepository.findByName(username);
+    }
+}
